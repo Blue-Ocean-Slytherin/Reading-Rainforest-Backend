26919439704a1626e57386b17d2db2a6fa0b01fe
dGarcia-NC <anthonygarcia833@gmail.com>
2022-10-04 16:31:10 -0700

    Fixed search route

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const db = require("./db/schema");
+var cors = require("cors");
 const app = express();
 const morgan = require("morgan");
 const Router = require("express");
@@ -8,6 +9,7 @@ const controllers = require("./routes/controllers.js");
 const searchRouter = require("./routes/search.js");
 const userRouter = require("./routes/user.js");
 
+app.use(cors());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms")
 );
@@ -15,40 +17,11 @@ app.use(express.json());
 
 app.use("/search", searchRouter);
 app.use("/user/", userRouter);
-// app.get("/initSearchBooks", (req, res) => {
-//   db.getData()
-//     .then((result) => {
-//       res.status(200).send(result);
-//     })
-//     .catch((error) => {
-//       res.status(400).send("GET from server hit an error");
-//     });
-// });
 
-// app.get("/books/:searchInput", (req, res) => {
-//   axios
-//     .get(`https://www.googleapis.com/books/v1/volumes?q=${req.params}`)
-//     .then((response) => {
-//       var onlyISBN = [];
-//       response.data.items.forEach((isbn) => {
-//         if (isbn.volumeInfo.industryIdentifiers[0] !== undefined) {
-//           onlyISBN.push(isbn.volumeInfo.industryIdentifiers[0].identifier);
-//         }
-//         if (isbn.volumeInfo.industryIdentifiers[1] !== undefined) {
-//           onlyISBN.push(isbn.volumeInfo.industryIdentifiers[1].identifier);
-//         }
-//       });
-//       console.log("bookISBN:", onlyISBN);
-//       res.status(200).json(onlyISBN);
-//       // TODO: Make axios request to DB, send onlyISBN as params
-//       // TODO: Make new route that takes in onlyISBN and responds with array of book data that exist from DB
-//     })
-//     .catch((error) => console.log(error));
-// });
 // app.get("/user/subs", controllers.getUserSub); // temp, just to see what's in DB
 
 // app.get("/user/:userSub/books", controllers.getUserBooks);
 
-app.listen(3001, () => {
-  console.log(`Listening at http://localhost:3001`);
+app.listen(3002, () => {
+  console.log(`Listening at http://localhost:3002`);
 });
--- server/models/search.js
+++ server/models/search.js
@@ -4,4 +4,12 @@ module.exports = {
   getInitialBooks: () => {
     return User.find();
   },
+  searchBooks: async (params) => {
+    var prom = await Promise.all(
+      params.map((isbn) => {
+        return User.find({ books: isbn }).exec();
+      })
+    );
+    return prom;
+  },
 };
