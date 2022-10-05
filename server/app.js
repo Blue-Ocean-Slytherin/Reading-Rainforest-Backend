6f30f5ee5fb21c934c4a58286e8720b765248d61
Sean Young <SeanKaicheeYoung@gmail.com>
2022-10-04 17:27:34 -0700

    Fixed cors issue with FE & BE communication

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require("./db/schema");
 const app = express();
+const cors = require('cors');
 const morgan = require("morgan");
 const Router = require("express");
 const axios = require("axios").default;
@@ -11,6 +12,8 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms")
 );
 app.use(express.json());
+app.use(cors());
+
 
 app.use("/search", searchRouter);
 app.use("/user/", userRouter);
