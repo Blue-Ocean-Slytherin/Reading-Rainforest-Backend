daf40a84fbc997a5d678db6757b174615bfc332d
Sean Young <SeanKaicheeYoung@gmail.com>
2022-10-06 12:20:28 -0700

    Added deleteBooks route to remove books from user's list. Not sure where we'll implement this rn, but it's going to be necessary for trade features

--- server/models/user.js
+++ server/models/user.js
@@ -71,9 +71,20 @@ module.exports = {
 
   deleteBook: async ( uid, ISBN ) => {
     try {
-      let userInfo = await getUserInfo(uid);
-      console.log(userInfo);
-      return;
+      let userInfo = await User.find({uid});
+      let bookList = userInfo[0]['books'];
+      for ( let i = 0; i < bookList.length; i++ ) {
+        if ( bookList[i]['isbn'] === ISBN ) {
+          bookList.splice(i,1);
+          break;
+        }
+      }
+      let updatedUserInfo = await User.findOneAndUpdate(
+        {uid},
+        {books: bookList },
+        {new:true}
+      );
+      return updatedUserInfo;
     } catch (err) {
       console.log('There was an error @ user/models.deleteBook', err);
       return null;
